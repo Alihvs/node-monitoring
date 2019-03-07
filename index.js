793d996a60c612a921a334e8e2c44bb7d12f20ce
Ali Havasi <a.havasi@alibaba.ir>
2019-03-07 17:33:07 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+/*
+* Primary file for the API
+*
+*
+*/
+
+// Dependicies
+var http = require('http');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+
+// The server should respond to all request with a string
+var server = http.createServer(function(req, res) {
+
+    // Get the URL and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path from that URL
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
+    // Get the HTTP method
+    var method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    var headers = req.headers;
+
+    // Get the payload if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data) {
+        buffer += decoder.write(data);
+    });
+    req.on('end', function() {
+        buffer += decoder.end();
+
+        // Send the response
+        res.end('Hello world\n');
+    
+        // Log the request path
+        console.log('Payload: ', buffer);
+    })
+});
+
+// Start the server and have it listen on port 3000
+
+server.listen(3000, function() {
+    console.log('The server is listening on port 3000 now');
+});
